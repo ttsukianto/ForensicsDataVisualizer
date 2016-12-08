0838405b737240a9e71c0803e50f2747bd292183
ttsukianto <mg.senryaku@gmail.com>
2016-12-07 17:06:04 -0700

    Cardinal spline graph

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -2,7 +2,12 @@
 //https://bl.ocks.org/mbostock/3887118
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 40
+    },
     width = 800 - margin.left - margin.right,
     height = 150 - margin.top - margin.bottom;
 
@@ -29,77 +34,82 @@ var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .attr("style", "outline: solid black;")
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 //Change "scatterplot.xml" to the XML file you want to parse
-    d3.xml("sample data/scatterplot.xml", function(error, data) {
-      if (error) throw error;
-      data = [].map.call(data.querySelectorAll("suspect"), function(suspect) {
+d3.xml("sample data/scatterplot.xml", function(error, data) {
+    if (error) throw error;
+    data = [].map.call(data.querySelectorAll("suspect"), function(suspect) {
         return {
-          suspect: suspect.getAttribute("id"),
-          suspectMaleProbability: +suspect.querySelector("suspectMaleProbability").textContent
+            suspect: suspect.getAttribute("id"),
+            suspectMaleProbability: +suspect.querySelector("suspectMaleProbability").textContent
         }
-      });
-      //loop running through each data value
-        data.forEach(function(d) {
-          d.suspectMaleProbability = +d.suspectMaleProbability;
-        });
+    });
+    //loop running through each data value
+    data.forEach(function(d) {
+        d.suspectMaleProbability = +d.suspectMaleProbability;
+    });
 
-//Set domains
-  x.domain([0,100]).nice();
-  x2.domain([100,0]).nice();
+    //Set domains
+    x.domain([0, 100]).nice();
+    x2.domain([100, 0]).nice();
 
-//add x axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    //x axis label
-    .append("text")
-      .attr("class", "label")
-      .attr("x", 0)
-      .attr("y", -6)
-      .style("text-anchor", "start")
-      .text("Male");
-
-  svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + 10 + ")")
-          .call(x2Axis)
+    //add x axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
         //x axis label
         .append("text")
-          .attr("class", "label")
-          .attr("x", 0)
-          .attr("y", 12)
-          .style("text-anchor", "start")
-          .text("Female");
+        .attr("class", "label")
+        .attr("x", 0)
+        .attr("y", -6)
+        .style("text-anchor", "start")
+        .text("Male Probability (%)");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + 10 + ")")
+        .call(x2Axis)
+        //x axis label
+        .append("text")
+        .attr("class", "label")
+        .attr("x", 0)
+        .attr("y", 12)
+        .style("text-anchor", "start")
+        .text("Female Probability (%)");
 
 
-//Scatterplot dots
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-    //Type of dot
-      .attr("class", "dot")
-      //Size of dot
-      .attr("r", 3.5)
-      //Parses x value from the data
-      .attr("cx", function(d) { return x(d.suspectMaleProbability); })
-      .attr("cy", 50)
-      //Chooses color based on data category
-      .style("fill", function(d) { return color(d.suspect); })
-      .each(function() {
+    //Scatterplot dots
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        //Type of dot
+        .attr("class", "dot")
+        //Size of dot
+        .attr("r", 3.5)
+        //Parses x value from the data
+        .attr("cx", function(d) {
+            return x(d.suspectMaleProbability);
+        })
+        .attr("cy", 50)
+        //Chooses color based on data category
+        .style("fill", function(d) {
+            return color(d.suspect);
+        })
+        .each(function() {
             var sel = d3.select(this);
             var state = false;
             sel.on('click', function() {
-              state = !state;
-              if (state) {
-                sel.style('fill', 'gray');
-              }
-              else {
-                sel.style('fill', function(d) { return color(d.suspect); });
-              }
+                state = !state;
+                if (state) {
+                    sel.style('fill', 'white');
+                } else {
+                    sel.style('fill', function(d) {
+                        return color(d.suspect);
+                    });
+                }
             });
-      });
-    });
+        });
+});
